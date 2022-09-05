ac4b2604f38338f88266be39fb9dd70c531ee1ed
Sith14 <109386708+awesomefamous@users.noreply.github.com>
2022-09-05 17:06:57 +0200

    Create javascript.js

---
+++ public/javascript.js
@@ -0,0 +1,15 @@
+// /public/javascript.js
+
+// Get the current username from the cookies
+var user = cookie.get('user');
+if (!user) {
+
+  // Ask for the username if there is none set already
+  user = prompt('Choose a username:');
+  if (!user) {
+    alert('We cannot work with you like that!');
+  } else {
+    // Store it in the cookies for future use
+    cookie.set('user', user);
+  }
+}
